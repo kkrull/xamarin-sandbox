85996e67eb563099ba600fe1a2dce89f0793a90f
Kyle Krull <kkrull@8thlight.com>
2018-12-04 15:07:15 -0600

    Add unit tests
    https://docs.microsoft.com/en-us/xamarin/ios/deploy-test/touch.unit

---
+++ Phoneword_iOS.Test/TranslatorTest.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using NUnit.Framework;
+
+namespace Phoneword_iOS.Test
+{
+    [TestFixture]
+    public class TranslatorTest
+    {
+        [Test]
+        public void Pass()
+        {
+            Assert.True(true);
+        }
+
+        [Test]
+        public void Fail()
+        {
+            Assert.False(true);
+        }
+
+        [Test]
+        [Ignore("another time")]
+        public void Ignore()
+        {
+            Assert.True(false);
+        }
+    }
+}
