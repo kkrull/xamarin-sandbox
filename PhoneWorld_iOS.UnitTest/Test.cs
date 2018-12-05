1ddceff2a597ae5e45b57d44499e1be52f5a0fd6
Kyle Krull <kkrull@8thlight.com>
2018-12-04 19:57:35 -0600

    Add plain NUnit 3 project for non-UI tests

---
+++ PhoneWorld_iOS.UnitTest/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+namespace PhoneWorld_iOS.UnitTest
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+            var answer = 42;
+            Assert.AreEqual(42, answer);
+        }
+    }
+}
