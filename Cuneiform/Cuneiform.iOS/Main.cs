bca2928b087084711fd8ffd44f4c5354a72a6d75
Kyle Krull <kkrull@8thlight.com>
2018-12-14 14:22:33 -0600

    Cleanup iOS

--- Cuneiform/Cuneiform.iOS/Main.cs
+++ Cuneiform/Cuneiform.iOS/Main.cs
@@ -1,20 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
-using UIKit;
+﻿using UIKit;
 
 namespace Cuneiform.iOS
 {
-    public class Application
+  public static class Application
+  {
+    public static void Main(string[] args)
     {
-        // This is the main entry point of the application.
-        static void Main(string[] args)
-        {
-            // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
-        }
+      UIApplication.Main(args, null, "AppDelegate");
     }
+  }
 }
