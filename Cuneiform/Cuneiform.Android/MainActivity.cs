ee34ec311ed75d8560b62c666cc8c31ce29c45a2
Kyle Krull <kkrull@8thlight.com>
2018-12-14 13:45:40 -0600

    Cleanup Android

--- Cuneiform/Cuneiform.Android/MainActivity.cs
+++ Cuneiform/Cuneiform.Android/MainActivity.cs
@@ -1,25 +1,28 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
 namespace Cuneiform.Droid
 {
-    [Activity(Label = "Cuneiform", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+  [Activity(
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+    Icon = "@mipmap/icon",
+    Label = "Cuneiform",
+    MainLauncher = true,
+    Theme = "@style/MainTheme"
+  )]
+  public class MainActivity : FormsAppCompatActivity
+  {
+    protected override void OnCreate(Bundle savedInstanceState)
     {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+      TabLayoutResource = Resource.Layout.Tabbar;
+      ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
-        }
+      base.OnCreate(savedInstanceState);
+      Forms.Init(this, savedInstanceState);
+      LoadApplication(new App());
     }
+  }
 }
