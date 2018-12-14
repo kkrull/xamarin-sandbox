1413663e387eb9e3f09729d7ccf1b06014597a04
Kyle Krull <kkrull@8thlight.com>
2018-12-14 13:40:28 -0600

    Cleanup Cuneiform core

--- Cuneiform/Cuneiform/App.xaml.cs
+++ Cuneiform/Cuneiform/App.xaml.cs
@@ -1,32 +1,28 @@
-﻿using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
 namespace Cuneiform
 {
-    public partial class App : Application
+  public partial class App : Application
+  {
+    public App()
     {
-        public App()
-        {
-            InitializeComponent();
-
-            MainPage = new MainPage();
-        }
-
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
-        }
+      InitializeComponent();
+      MainPage = new MainPage();
     }
+
+    protected override void OnStart()
+    {
+    }
+
+    protected override void OnSleep()
+    {
+    }
+
+    protected override void OnResume()
+    {
+    }
+  }
 }
--- Cuneiform/README.md
+++ Cuneiform/README.md
@@ -9,3 +9,25 @@ But hang tight, folks, it's gonna be great.
 [cuneiform-og]: https://en.wikipedia.org/wiki/Cuneiform
 [xamarin-forms]: https://docs.microsoft.com/en-us/xamarin/xamarin-forms/
 
+
+## Build
+
+Build in Debug mode, targeting the iPhoneSimulator platform.
+
+
+## Running Android on the Android Emulator
+
+Target a device with a [sufficient version of Android][xamarin-requirements] (19 or later, as of this writing).
+
+Make sure you're targeting the `iPhoneSimulator`.  
+Running in debug mode, on Android, targeting other platforms such as `Any CPU` seems to result in an endless stream of
+log messages about thread priorities.
+
+Note also that `docker-engine` may need to be stopped, at least if you are running from Visual Studio for Mac.
+
+[xamarin-requirements]: https://docs.microsoft.com/en-us/xamarin/xamarin-forms/get-started/installation
+
+
+## Running iOS on the iOS Simulator
+
+The sample application seems to run well on iPhone 7 (iOS 12.1).
